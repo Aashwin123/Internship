bdb5930323d1ab05e9051de4ec9fe400db72c1c4
Aashwin123 <Aashwinpant25@gmail.com>
2025-08-26 16:29:12 +0545

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+import ChildComponent from './components/ChildComponent.vue'
+
+const showChild = ref(true)
+
+function toggleChild(){
+  showChild.value =!showChild.value
+}
+</script>
+
+<template>
+  <div>
+  <h1>Vue LifeCycle Demo</h1>
+  <ChildComponent v-if="showChild" />
+  <button @click="toggleChild">Toggle Child Component</button>
+  </div>
+</template>
+
+
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <button @click="increment">Increment: {{ count }}</button>
+  </div>
+</template>
+
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+const count = ref(0)
+function increment() {
+  count.value++
+}
+
+console.log(" setup() called - component instance created")
+
+onBeforeMount(() => {
+  console.log(" onBeforeMount - DOM not yet rendered")
+})
+
+onMounted(() => {
+  console.log(" onMounted - DOM rendered, good for API calls")
+})
+
+onBeforeUpdate(() => {
+  console.log(" onBeforeUpdate - before DOM changes")
+})
+
+onUpdated(() => {
+  console.log(" onUpdated - after DOM changes")
+})
+
+onBeforeUnmount(() => {
+  console.log(" onBeforeUnmount - before component is removed")
+})
+
+onUnmounted(() => {
+  console.log(" onUnmounted - component destroyed, cleanup here")
+})
+</script>
